c2b015847cb4c546f10145c9e387e290b27271e8
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-23 18:34:13 +0100

    [fix] Chest

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class ChestScript : Interactive
 {
     public Animator animator;
+    public GameObject SilverKey;
 
     public override void OnInteraction()
     {
         //If I want to do the base OnInteraction anyway first
         //
         //Activate Chest & Key
-        GetComponent<Animator>().SetTrigger("ChestOpen");
-
+        GetComponent<Animator>().Play("Open Chest Lid");
+        SilverKey.SetActive(true);
+        GetComponent<Animator>().SetTrigger("ChestOpened");
     }
 }
