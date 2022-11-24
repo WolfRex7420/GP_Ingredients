57f43ebf2fd909140296d8f72960c6ff55c06dec
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-24 17:22:17 +0100

    Jail Door

---
+++ Assets/Scripts/Marie/Items/Interactive/JailDoor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JailDoor : Interactive
+{
+    //Basic behaviour for Interactive objects is to trigger an animation
+    //virtual makes the function changeable in children classes
+    public override void OnInteraction()
+    {
+        GetComponent<Animator>().SetTrigger("JailDoorOpen");
+    }
+}
