7ffc60718bc1ea69991cd4178bcde2acf84be4d8
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-25 15:47:26 +0100

    [fix] second room (final)

--- Assets/Scripts/Marie/Items/Interactive/SecondDoor.cs
+++ Assets/Scripts/Marie/Items/Interactive/SecondDoor.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : Interactive
+public class SecondDoor : Interactive
 {
-    public GameObject Key_Golden;
-    public GameObject Key_Rusty;
     //Basic behaviour for Interactive objects is to trigger an animation
     //virtual makes the function changeable in children classes
     public override void OnInteraction()
     {
-        GetComponent<Animator>().SetTrigger("Switch");
-        Key_Golden.SetActive(true);
-        Key_Rusty.SetActive(true);
+        if (requiredItem)
+        {
+            GetComponent<Animator>().SetTrigger("SecondDoorOpen");
+        }
+        GetComponent<Animator>().SetTrigger("Failed");
     }
 }
